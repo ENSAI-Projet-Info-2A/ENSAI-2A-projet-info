af05f12fad9765227946aa3bd647f29c7591b478
id2825 <simon.willot@eleve.ensai.fr>
2025-09-24 18:29:24 +0000

    ajout du diagramme de gantt

---
+++ doc/diagramme_gantt.py
@@ -0,0 +1,17 @@
+gantt
+    title Projet Info
+    dateFormat  MM-DD
+    section Phase 1
+    Analyse du sujet     :a1, 08-29, 7d
+    section Phase 2   
+    Conception diagrammes:a2, after a1, 14d
+    Rédaction:a3,09-19,7d
+    section Phase 3
+    Arborescence code:a4, after a3, 12d
+    squelette classes:a5,09-29,14d
+    Création des méthodes:a6,after a5,20d
+    Implémentation des tests :a7, after a5, 25d
+    Rédaction:a8,11-05,14d
+    Relecture:a9,after a8,4d
+    Rendu rapport d'analyse      :milestone, after a3, 1d
+    Rendu final du projet        :milestone, 11-22, 1d
