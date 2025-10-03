cee3df8875f67d65bfde71ff4043fed4f3944b30
mroux <maxime.roux@eleve.ensai.fr>
2025-10-03 10:51:32 +0000

    Fix : Tentative d'ajout de l'initinalisation de la DB

--- data/init_db.sql
+++ data/init_db.sql
@@ -1,12 +1,22 @@
 -----------------------------------------------------
 -- Joueur
 -----------------------------------------------------
-DROP TABLE IF EXISTS joueur CASCADE ;
-CREATE TABLE joueur(
-    id_joueur    SERIAL PRIMARY KEY,
-    pseudo       VARCHAR(30) UNIQUE,
-    mdp          VARCHAR(256),
-    age          INTEGER,
-    mail         VARCHAR(50),
-    fan_pokemon  BOOLEAN
+
+-- DROP TABLE IF EXISTS joueur CASCADE ;
+-- CREATE TABLE joueur(
+--     id_joueur    SERIAL PRIMARY KEY,
+--     pseudo       VARCHAR(30) UNIQUE,
+--     mdp          VARCHAR(256),
+--     age          INTEGER,
+--     mail         VARCHAR(50),
+--     fan_pokemon  BOOLEAN
+-- );
+
+-- Table attendue
+-- schema.public.utilisateur (ou ton schema .env)
+-- id SERIAL PK, pseudo UNIQUE, password_hash TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS utilisateur (
+  id             SERIAL PRIMARY KEY,
+  pseudo         TEXT UNIQUE NOT NULL,
+  password_hash  TEXT NOT NULL
 );
