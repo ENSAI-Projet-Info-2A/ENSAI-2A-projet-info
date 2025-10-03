ad73f74b1874694ec7002cb7daab6da135a0d65f
id2825 <simon.willot@eleve.ensai.fr>
2025-10-03 14:26:16 +0000

    db

--- data/init_db.sql
+++ data/init_db.sql
@@ -24,21 +24,26 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS messages  (
   id             SERIAL PRIMARY KEY,
   conversation_id        TEXT UNIQUE NOT NULL,
-  user_id TEXT NOT NULL,
-  sender_type ,
-  content,
-  created_at
+  user_id  INT NULL,
+  sender_type ENUM('user', 'ai') NOT NULL,
+  content TEXT NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (conversation_id) REFERENCES conversations(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS conversations (
   id             SERIAL PRIMARY KEY,
-  pseudo         TEXT UNIQUE NOT NULL,
-  password_hash  TEXT NOT NULL
+  title         TEXT NOT NULL,
+  created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS conversations_participants (
   id             SERIAL PRIMARY KEY,
-  pseudo         TEXT UNIQUE NOT NULL,
-  password_hash  TEXT NOT NULL
+  conversation_id   INT NULL  ,
+  user_id  INT NULL,
+  FOREIGN KEY (conversation_id) REFERENCES conversations(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  UNIQUE(conversation_id, user_id) 
 );
 
