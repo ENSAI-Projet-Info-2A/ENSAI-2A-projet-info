0f72d61915146e89ab792f226219bd645a865c91
mroux <maxime.roux@eleve.ensai.fr>
2025-11-18 23:13:26 +0000

    Fix : Ajout de la gestion des déconnexions par intéruption

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ import logging
 
 import dotenv
 
+from src.dao.utilisateur_dao import UtilisateurDao
+from src.service.auth_service import Auth_Service
+
+# Ajouts pour la déconnexion si l'utilisateur fait un ctrl+c
+from src.view.session import Session
 from utils.log_init import initialiser_logs
 from view.accueil.accueil_vue import AccueilVue
 
@@ -14,22 +19,49 @@ if __name__ == "__main__":
     vue_courante = AccueilVue("Bienvenue")
     nb_erreurs = 0
 
-    while vue_courante:
-        if nb_erreurs > 100:
-            print("Le programme recense trop d'erreurs et va s'arrêter")
-            break
-        try:
-            # Affichage du menu
-            vue_courante.afficher()
+    try:
+        while vue_courante:
+            if nb_erreurs > 100:
+                print("Le programme recense trop d'erreurs et va s'arrêter")
+                break
+            try:
+                # Affichage du menu
+                vue_courante.afficher()
 
-            # Affichage des choix possibles
-            vue_courante = vue_courante.choisir_menu()
-        except Exception as e:
-            logging.info(e)
-            nb_erreurs += 1
-            vue_courante = AccueilVue("Une erreur est survenue, retour au menu principal")
+                # Affichage des choix possibles
+                vue_courante = vue_courante.choisir_menu()
 
-    # Lorsque l on quitte l application
+            except Exception as e:
+                logging.info(e)
+                nb_erreurs += 1
+                vue_courante = AccueilVue("Une erreur est survenue, retour au menu principal")
+
+    except KeyboardInterrupt:
+        # Gestion du Ctrl + C (arrêt propre)
+        print("\n\nInterruption détectée (Ctrl + C).")
+        print("Fermeture propre de la session...")
+
+        s = Session()
+        if s.utilisateur:
+            # Invalidation du token si nécessaire
+            if s.token:
+                try:
+                    auth_service = Auth_Service(UtilisateurDao())
+                    auth_service.se_deconnecter(s.token)
+                except Exception:
+                    pass  # on n'arrête pas pour ça
+
+            # Fermeture de la session BDD + reset
+            try:
+                s.deconnexion()
+            except Exception:
+                pass
+
+            print("Session correctement terminée.")
+        else:
+            print("Aucune session ouverte.")
+
+    # Lorsque l on quitte l application (cas normal ou Ctrl+C)
     print("----------------------------------")
     print("Au revoir")
 
